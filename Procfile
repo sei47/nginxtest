1d1720d1c38dfdc349d859072ab16bbea0341f39
GGG <kuranberitorappu@gmail.com>
2022-05-16 08:20:17 +0000

    init

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
 web: scripts/build_nginx.sh
+web: bin/start-nginx bundle exec unicorn -c config/unicorn.rb
--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,30 +1,9 @@
-worker_processes 2
-
-# ソケット
-listen  '/tmp/unicorn.sock'
-pid     '/tmp/unicorn.pid'
-
-# ログ
-log = '${my_app}/log/unicorn.log'
-stderr_path File.expand_path('log/unicorn.log', ENV['RAILS_ROOT'])
-stdout_path File.expand_path('log/unicorn.log', ENV['RAILS_ROOT'])
-
+require 'fileutils'
 preload_app true
-GC.respond_to?(:copy_on_write_friendly=) and GC.copy_on_write_friendly = true
+timeout 5
+worker_processes 4
+listen '/tmp/nginx.socket', backlog: 1024
 
-before_fork do |server, worker|
-defined?(ActiveRecord::Base) and ActiveRecord::Base.connection.disconnect!
-
-old_pid = "#{ server.config[:pid] }.oldbin"
-unless old_pid == server.pid
-  begin
-   sig = (worker.nr + 1) >= server.worker_processes ? :QUIT : :TTOU
-   Process.kill :QUIT, File.read(old_pid).to_i
-   rescue Errno::ENOENT, Errno::ESRCH
-  end
-end
-end
-
-after_fork do |server, worker|
-    defined?(ActiveRecord::Base) and ActiveRecord::Base.establish_connection
+before_fork do |server,worker|
+  FileUtils.touch('/tmp/app-initialized')
 end
